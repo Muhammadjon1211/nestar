9fe87723891f50efe95ce578f2899526d822d004
john <mkushmonov@gmail.com>
2026-08-28 12:25:18 +0900

    feat: develop createProperty graphQL api

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import PropertySchema from '../../schemas/Property.model';
+import { MemberModule } from '../member/member.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: "Property", schema: PropertySchema }]),// PropertySchema model, => property resolverga yordam keladi
     AuthModule,
     ViewModule,
+    MemberModule,
   ],
   providers: [PropertyResolver, PropertyService]
 })
--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,13 +1,28 @@
-import { Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
+import { Property } from '../../libs/dto/property/property';
+import { PropertyInput } from '../../libs/dto/property/property.input';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { UseGuards } from '@nestjs/common';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import * as mongoose from 'mongoose';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
 
 @Resolver()
 export class PropertyResolver {
   constructor(private readonly propertyService: PropertyService) { }
 
-  @Mutation(() => Member) // POST
-  public async signup(@Args('input') input: MemberInput): Promise<Member> {
-    console.log("Mutation: signup");
-    return this.propertyService.signup(input);
+  @Roles(MemberType.AGENT)
+  @UseGuards(RolesGuard)
+  @Mutation(() => Property) // POST
+  public async createProperty(
+    @Args('input') input: PropertyInput,
+    @AuthMember("_id") memberId: mongoose.ObjectId
+  ): Promise<Property> {
+    console.log("Mutation: createProperty");
+    input.memberId = memberId;
+
+    return this.propertyService.createProperty(input);
   }
 }
