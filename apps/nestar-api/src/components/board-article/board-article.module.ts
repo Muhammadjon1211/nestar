f9751df71b8f72e46bb2ca01947036565beb53cf
john <mkushmonov@gmail.com>
2026-09-01 15:46:20 +0900

    feat: configure boardArticle module and integrate boardArticle DTO

--- apps/nestar-api/src/components/board-article/board-article.module.ts
+++ apps/nestar-api/src/components/board-article/board-article.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { BoardArticleResolver } from './board-article.resolver';
+import { BoardArticleService } from './board-article.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import BoardArticleSchema from '../../schemas/BoardArticle.model';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import { MemberModule } from '../member/member.module';
 
-@Module({})
-export class BoardArticleModule {}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: "BoardArticle",
+        schema: BoardArticleSchema
+      }
+    ]),// PropertySchema model, => property resolverga yordam keladi
+    AuthModule,
+    ViewModule,
+    MemberModule,
+  ],
+  exports: [BoardArticleService],
+  providers: [BoardArticleResolver, BoardArticleService]
+})
+export class BoardArticleModule { }
